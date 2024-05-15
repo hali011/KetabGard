36f568e8ff5fb95fa38f79437025b70c0c4fcbca
Ali <hasanpat.study@gmail.com>
2024-05-15 22:49:53 +0330

    Create some page (add html css and js)

---
+++ KetabGard/KetabGard/Controllers/HomeController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KetabGard.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Dashboard(string id)
+        {
+            int.TryParse(id, out int x);
+            if (x == 1)
+            {
+                return PartialView();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Userpage(string id)
+        {
+            int.TryParse(id, out int x);
+            if (x == 1)
+            {
+                return PartialView();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Bookpage(string id)
+        {
+            int.TryParse(id, out int x);
+            if (x == 1)
+            {
+                return PartialView();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Amanatpage(string id)
+        {
+            int.TryParse(id, out int x);
+            if (x == 1)
+            {
+                return PartialView();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Addadminpage(string id)
+        {
+            int.TryParse(id, out int x);
+            if (x == 1)
+            {
+                return PartialView();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult UsersTypeTable(int id)
+        {
+            return PartialView();
+        }
+        public ActionResult Adduserpage(string id)
+        {
+            int.TryParse(id, out int x);
+            if (x == 1)
+            {
+                return PartialView();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ KetabGard/KetabGard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KetabGard.MvcApplication" Language="C#" %>
