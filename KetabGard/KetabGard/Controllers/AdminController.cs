0b79db649143aebf7308ad7b53117983ca1fd358
Ali <hasanpat.study@gmail.com>
2024-05-21 10:28:17 +0330

    create all front just need database and backend

---
+++ KetabGard/KetabGard/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KetabGard.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Login(List<Userpass> userpass)
+        {
+            if (userpass[0].Username.ToLower() == "ali" && userpass[0].Password == "12345")
+            {
+                Session["Admin"] = userpass[0];
+                return RedirectToAction("Index","Home");
+            }
+            else
+            {
+                return Json("error");
+            }
+        }
+    }
+}
--- KetabGard/KetabGard/Controllers/HomeController.cs
+++ KetabGard/KetabGard/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,9 +9,21 @@ namespace KetabGard.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Login(string user , string pass)
         {
             return View();
+        }
+        public ActionResult Index()
+        {
+            var us = Session["Admin"] as Userpass; 
+            if (us != null)
+            {
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login");
+            }
         }
         public ActionResult Dashboard(string id)
         {
@@ -60,7 +73,7 @@ namespace KetabGard.Controllers
                 return RedirectToAction("Index");
             }
         }
-        public ActionResult Addadminpage(string id)
+        public ActionResult Adminpage(string id)
         {
             int.TryParse(id, out int x);
             if (x == 1)
@@ -87,6 +100,99 @@ namespace KetabGard.Controllers
             {
                 return RedirectToAction("Index");
             }
+        }
+        public ActionResult Addbookpage(string id)
+        {
+            int.TryParse(id, out int x);
+            if (x == 1)
+            {
+                return PartialView();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult Addadminpage(string id)
+        {
+            int.TryParse(id, out int x);
+            if (x == 1)
+            {
+                return PartialView();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        [HttpPost]
+        public JsonResult UploadImage()
+        {
+            try
+            {
+                if (Request.Files.Count > 0)
+                {
+                    var file = Request.Files[0];
+
+                    int x = Request.Files.Count;
+                    string uploadDir = "";
+                    switch (x)
+                    {
+                        case 1:
+                            uploadDir = Server.MapPath("~/AdminPic/");
+                            break;
+                        case 2:
+                            uploadDir = Server.MapPath("~/BookPic/");
+                            break;
+                        case 3:
+                            uploadDir = Server.MapPath("~/UserPic/");
+                            break;
+                    }
+                    if (!Directory.Exists(uploadDir))
+                    {
+                        Directory.CreateDirectory(uploadDir);
+                    }
+
+                    var filePath = Path.Combine(uploadDir, file.FileName);
+                    file.SaveAs(filePath);
+
+                    return Json(new { success = true });
+                }
+                else
+                {
+                    return Json(new { success = false, message = "No file uploaded" });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message });
+            }
+        }
+        public ActionResult AmanatiTypeTable(int id)
+        {
+            return PartialView();
+        }
+        public ActionResult Addamanatipage(string id)
+        {
+            int.TryParse(id, out int x);
+            if (x == 1)
+            {
+                return PartialView();
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+        public ActionResult booksearch(string bookname)
+        {
+            ViewBag.serachof = "کتاب";
+            return PartialView("Searchresult");
+        }
+        public ActionResult usersearch(string nameorcdm)
+        {
+            ViewBag.serachof = "عضو";
+            return PartialView("Searchresult");
         }
     }
 }
